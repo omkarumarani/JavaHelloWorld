f7dd4ca02a758cc729331731092cdb6a44545481
OnkarGitboy <omkar.umrani@gmail.com>
2023-12-07 07:11:23 +0530

    Update App.java

--- demo/src/main/java/firstproject/App.java
+++ demo/src/main/java/firstproject/App.java
@@ -1,4 +1,8 @@
 package firstproject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -6,6 +10,7 @@ package firstproject;
  */
 public class App 
 {
+    static List<String> favoriteFruits = new ArrayList<>(Arrays.asList("Greap", "pinapple","Guava"));
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
